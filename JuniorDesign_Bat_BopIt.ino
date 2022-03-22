54bb2bd8b42308eb071121b7f6ae165c484e2245
SSkyeG <60981354+SSkyeG@users.noreply.github.com>
2022-03-21 20:02:18 -0400

    Hello World Project Code
    Didn't make it the hello world project code oops

--- JuniorDesign_Bat_BopIt.ino
+++ JuniorDesign_Bat_BopIt.ino
@@ -1,9 +1,21 @@
+#define newLED 8
+#define button_pin 9
+
 void setup() {
   // put your setup code here, to run once:
-  printf("Hello World");
+  // put your setup code here, to run once:
+  pinMode(newLED, OUTPUT);
+  pinMode(button_pin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int button_state = digitalRead(button_pin);
+  if (button_state == HIGH) 
+  { 
+    digitalWrite(newLED, HIGH);
+    delay(10);
+    digitalWrite(newLED, LOW);
+    delay(10);
+  }
 }
