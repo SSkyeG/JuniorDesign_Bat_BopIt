03c565426d9176069a08379375fd00f8fe502f3b
SSkyeG <60981354+SSkyeG@users.noreply.github.com>
2022-03-29 12:25:16 -0400

    Compiles now
    hopefully a  functioning code if the arduino will work

--- JuniorDesign_Bat_BopIt.ino
+++ JuniorDesign_Bat_BopIt.ino
@@ -1,15 +1,15 @@
 
 //Input
-#define OnButton 1
+#define OnButton 8
 
 #define Flex_Right A4
-#define Flex_Left A1
+#define Flex_Left A5
 
-#define AudioSensor A5
+#define AudioSensor A3
 
-#define TiltSens 1
+#define TiltSens 7
 
-#define NoseButton 1
+#define NoseButton 4
 
 //Output
 #define SpeakerSound 1
@@ -19,12 +19,12 @@ bool isBatOn= false;
 int score = 0;
 int TimeLimit = 10;
 
-int SoundBase = 50;
-int dB_threshold= 20;
+int SoundBase = 800;
+int dB_threshold= 200;
 
-int LeftWingBase = 6000;
-int RightWingBase = 6000;
-int WingMove_threshold = 1500;
+int LeftWingBase = 700;
+int RightWingBase = 700;
+int WingMove_threshold = 200;
 
 int res_threshold = 5;
 
@@ -35,10 +35,10 @@ void setup()
   // put your setup code here, to run once:
   pinMode(AudioSensor, INPUT);
   pinMode(Flex_Right, INPUT);
-  //pinMode(Flex_Left, INPUT);
-  //pinMode(AudioSensor, INPUT);
-  //pinMode(TiltSens, INPUT);
-  //pinMode(NoseButton, INPUT);
+  pinMode(Flex_Left, INPUT);
+  pinMode(AudioSensor, INPUT);
+  pinMode(TiltSens, INPUT);
+  pinMode(NoseButton, INPUT);
   pinMode(8, OUTPUT);
   //pinMode(SpeakerSound, OUTPUT);
   Serial.begin(9600); 
@@ -47,38 +47,43 @@ void setup()
 
 void loop() 
 {
+  /*
   // put your main code here, to run repeatedly:
-  int a = analogRead(Flex_Right);
-  Serial.print(analogRead(Flex_Right));
-  Serial.print('\n');
-  if(a>700 || a<400)
+  int a = analogRead(AudioSensor);
+  if(a>1000)
   {
-    digitalWrite(8,HIGH);  
+    Serial.print(analogRead(AudioSensor));
+    Serial.print('\n');
   }
-  else
+  getBases();
+  Serial.print("Start!");
+  while(true)
   {
-    digitalWrite(8, LOW);
+    
+    if(CheckBendIt())
+    {
+      Serial.print(analogRead(Flex_Right));
+      Serial.print(" Bent with ");
+      Serial.print(RightWingBase);
+      Serial.print("!\n");
+      break;
+    }
   }
-  /*int button_state = digitalRead(button_pin);
-  if (button_state == HIGH) 
-  { 
-    digitalWrite(newLED, HIGH);
-    delay(10);
-    digitalWrite(newLED, LOW);
-    delay(5);//Sierra's Change!
-  }
-  */
   
+  delay(3000);
+  */
   if (OnButton == HIGH && !isBatOn) 
   { 
     isBatOn= true;
     score = 0;
+    Serial.print("Game ON\n");
   }
   if(isBatOn)
   {
+    delay(2000);
     isBatOn = CallCommand();
   }
-  */
+  
 }
 
 bool CallCommand()
@@ -93,11 +98,14 @@ bool CallCommand()
     int randNumber = random(1, 5);
     
     //TODO: Output command for user to start;
+    int code=5;
     PlaySound(randNumber);
     
     //Get Base for Audio and Flex
     getBases();
-
+    
+    int Time = 0;
+    
     //In time limit...
     while(Time < TimeLimit) 
     {
@@ -187,7 +195,7 @@ bool CheckBoopIt()
 bool CheckScreamIt()
 {
   //poll for if task completed
-  soundinput = analogRead(AudioSensor);
+  int soundinput = analogRead(AudioSensor);
   if (soundinput > SoundBase+dB_threshold)
   {
     return true;
@@ -198,7 +206,7 @@ bool CheckScreamIt()
 bool CheckTiltIt ()
 {
   //poll for if task completed
-  movement = digitalRead(TiltSens);
+  int movement = digitalRead(TiltSens);
   if (movement ==HIGH)
   {
     return true;
@@ -209,8 +217,8 @@ bool CheckTiltIt ()
 bool CheckBendIt()
 {
   //Poll for if task completed
-  RightBent = analogRead(Flex_Right);
-  LeftBent = analogRead(Flex_Left);
+  int RightBent = analogRead(Flex_Right);
+  int LeftBent = analogRead(Flex_Left);
   
   if (LeftBent > LeftWingBase+WingMove_threshold || LeftBent < LeftWingBase-WingMove_threshold 
     || RightBent > RightWingBase+WingMove_threshold || RightBent < RightWingBase-WingMove_threshold)
@@ -222,24 +230,27 @@ bool CheckBendIt()
 
 void getBases()
 {
-  int audioSum =0;
+  //int audioSum =0;
   int wingRSum =0;
   int wingLSum =0;
   int times=15;
   for(int i = 0; i<times; i++)
   {
-    audioSum+=analogRead(AudioSensor);
+    //audioSum+=analogRead(AudioSensor);
     wingRSum+=analogRead(Flex_Right);
     wingLSum+=analogRead(Flex_Left);
   }
-  SoundBase = audioSum/times;
+  //SoundBase = audioSum/times;
   LeftWingBase = wingLSum/times;
   RightWingBase = wingRSum/times;
+  //Serial.print(RightWingBase);
+  //Serial.print("\n");
 }
 
 void Win()
 {
   //Output Winner audio;
+  int code=5;
   PlaySound(code);
   
   //Output score of 99 audio;
@@ -249,6 +260,8 @@ void Win()
 void Lose()
 {
   //Output Loser audio;
+  Serial.print("LOST\n");
+  int code=5;
   PlaySound(code);
   
   //Output score of user;
@@ -263,8 +276,23 @@ void TaskCompleted()
   CallCommand(); //go back to function to generate
 }
 
-void PlaySound(code)
+void PlaySound(int code)
 {
   //TODO: Send code for certain sounds to speaker
+
+  switch (code)
+  {
+    case 1:
+      Serial.print("Boop it\n");
+      break;
+    case 2:
+      Serial.print("Clap it\n");
+      break;
+    case 3:
+      Serial.print("Tilt it\n");
+      break;
+    case 4:
+      Serial.print("Bend it\n");
+      break;
+  }   
 }
-*/
